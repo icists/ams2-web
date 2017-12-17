932cdea7661a3f32d5cc470cb3be763113198f4a
Gunwoo Kim <gunwooterry@gmail.com>
2017-12-17 18:07:18 +0900

    init: setup element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
 
 /* eslint-disable no-new */
 new Vue({
