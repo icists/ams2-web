39cd98f6571d3228564f18e8495730a059c29892
Gunwoo Kim <gunwooterry@gmail.com>
2018-02-13 12:28:22 +0900

    feat: add AppHeader component

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1
+    class="header"
+    :style="style"
+  >
+    {{text}}
+  </h1>
+</template>
+
+<script>
+  export default {
+    name: 'AppHeader',
+
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+      color: {
+        type: String,
+        default: '#000',
+      },
+      size: {
+        type: Number,
+        default: 2,
+      },
+    },
+
+    data() {
+      return {
+        style: {
+          color: this.color,
+          fontSize: `${this.size}rem`,
+        },
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .header {
+    letter-spacing: -0.8px;
+    font-weight: bolder;
+  }
+</style>
