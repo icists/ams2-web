9c0fe73b005fdf9a68148e0656f9c3e566d5d9a9
Gunwoo Kim <gunwooterry@gmail.com>
2018-02-13 12:16:27 +0900

    feat: add grid components

---
+++ src/components/common/BaseRow.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="before"></div>
+    <mq-layout mq="xs" class="row xs">
+      <slot/>
+    </mq-layout>
+    <mq-layout mq="sm" class="row sm">
+      <slot/>
+    </mq-layout>
+    <mq-layout mq="md" class="row md">
+      <slot/>
+    </mq-layout>
+    <mq-layout mq="lg" class="row lg">
+      <slot/>
+    </mq-layout>
+    <div class="after"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseRow',
+  };
+</script>
+
+<style scoped>
+  .row {
+    max-width: 1000px;
+    zoom: 1;
+    margin: auto;
+  }
+
+  .before {
+    display: table;
+  }
+
+  .after {
+    clear: both;
+    display: table;
+  }
+
+  .xs {
+    padding-left: 0;
+    padding-right: 0;
+  }
+
+  .sm {
+    padding-left: 2%;
+    padding-right: 2%;
+  }
+
+  .md {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+
+  .lg {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueMq from 'vue-mq';
 import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -9,6 +10,14 @@ import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueMq, {
+  breakpoints: {
+    xs: 768,
+    sm: 992,
+    md: 1200,
+    lg: Infinity,
+  },
+});
 Vue.use(ElementUI, { locale });
 
 /* eslint-disable no-new */
